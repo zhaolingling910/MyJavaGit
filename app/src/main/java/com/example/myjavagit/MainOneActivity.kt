ecf48196f4f8fdb885e43654e0558c60a731ebc9
Your Name <you@example.com>
2024-03-22 13:44:49 +0800

    添加了一个返回按钮

--- app/src/main/java/com/example/myjavagit/MainOneActivity.kt
+++ app/src/main/java/com/example/myjavagit/MainOneActivity.kt
@@ -1,21 +1,29 @@
 package com.example.myjavagit
 
 
-import com.example.myjavagit.databinding.MainLayoutBinding
+import android.view.View
 import com.example.myjavagit.databinding.MainOneLayoutBinding
 
-class MainOneActivity : BaseActivity<MainOneLayoutBinding>() {
+class MainOneActivity : BaseActivity<MainOneLayoutBinding>(), View.OnClickListener {
     override fun getLayoutResId(): Int {
         return R.layout.main_one_layout
     }
 
     override fun initView() {
         mViewDataBinding.text.text = "跳转到了第二个页面 哈哈哈"
+        mViewDataBinding.button.setOnClickListener(this)
     }
 
     override fun initData() {
 
     }
 
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.button -> {
+                finish()
+            }
+        }
+    }
 }
 
