05374d5b6cae5c685bc502b65f49d8193c342f7c
Your Name <you@example.com>
2024-03-22 11:58:43 +0800

    提交第一版

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
--- app/src/main/java/com/example/myjavagit/MainActivity.kt
+++ app/src/main/java/com/example/myjavagit/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.example.myjavagit
 
 
+import android.content.Intent
+import android.view.View
 import com.example.myjavagit.databinding.MainLayoutBinding
 
-class MainActivity : BaseActivity<MainLayoutBinding>() {
+class MainActivity : BaseActivity<MainLayoutBinding>(), View.OnClickListener {
     override fun getLayoutResId(): Int {
         return R.layout.main_layout
     }
 
     override fun initView() {
         mViewDataBinding.text.text = "使用git ，练习git命令"
+        mViewDataBinding.button.setOnClickListener(this)
     }
 
     override fun initData() {
 
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.button -> {
+                // 创建一个意图来启动另一个活动
+                val intent = Intent(this, MainOneActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
 }
